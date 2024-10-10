db3f7e28c4a91bf81d84fac736ec576ca265a339
Fabio Zendhi Nagao <nagaozen@hotmail.com>
2024-10-10 05:11:30 -0300

    docs: `createPassword` jsdoc

--- src/lib/createPassword.mjs
+++ src/lib/createPassword.mjs
@@ -5,6 +5,15 @@ export const LOWERS = 'abcdefghijklmnopqrstuvwxyz'
 export const UPPERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 export const SYMBOLS = '~`! @#$%^&*()-_+={}[]|\\;:\'"<>,./?'
 
+/**
+ * Generates a random password string from the specified vocabulary and length.
+ *
+ * @param {string} [vocabulary=DIGITS + LOWERS + UPPERS + SYMBOLS] - A string containing the set of characters to use in the password.
+ * @param {number} length - The length of the password to generate.
+ * @returns {string} A randomly generated password string.
+ * @throws {TypeError} If the vocabulary is not a non-empty string.
+ * @throws {TypeError} If the length is not a positive integer.
+ */
 export function createPassword (vocabulary = DIGITS + LOWERS + UPPERS + SYMBOLS, length) {
   if (typeof vocabulary !== 'string' || vocabulary.length === 0) { throw new TypeError('Vocabulary must be a non-empty string') }
   if (!Number.isInteger(length) || length <= 0) { throw new TypeError('Length must be a positive integer') }
